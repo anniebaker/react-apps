eef33cfd328a1a95e6ae8f18d942d1ae19145811
anniebaker <bakeranneabb@gmail.com>
2019-06-02 22:27:55 -0500

    Updates

---
+++ precache-manifest.2965472d9ba73e92eaa605dccdd9a5f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51757141e277029dd3c5ed5d0536de71",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "4f9d6e6e4c43ba5b2a62",
+    "url": "/react-gh-pages/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react-gh-pages/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "4f9d6e6e4c43ba5b2a62",
+    "url": "/react-gh-pages/static/js/main.acdf2d60.chunk.js"
+  },
+  {
+    "revision": "a0c43bde80f322238324",
+    "url": "/react-gh-pages/static/js/runtime~main.ac2d5040.js"
+  }
+]);
